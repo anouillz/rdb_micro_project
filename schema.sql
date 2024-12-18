d2d783789679f5d55d3816c27440912795b5db4c
Tokxik <95232629+tirexellent@users.noreply.github.com>
2024-12-18 10:40:39 +0100

    Updated/working schema.sql

--- schema.sql
+++ schema.sql
@@ -1,63 +1,60 @@
-CREATE TABLE "sector" (
-  "id" integer PRIMARY KEY,
-  "name" varchar,
-  "description" varchar
+CREATE TABLE sector (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT
 );
+
+CREATE TABLE grade (
+  id SERIAL PRIMARY KEY,
+  grade_score VARCHAR(10) NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE subsector (
+  id SERIAL PRIMARY KEY,
+  id_sector INTEGER,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  FOREIGN KEY (id_sector) REFERENCES sector(id)
+);
+
+CREATE TABLE boulder (
+  id SERIAL PRIMARY KEY,
+  id_subsector INTEGER,
+  id_grade INTEGER,
+  condition INTEGER check (condition>0),
+  coordinates VARCHAR(255),
+  FOREIGN KEY (id_subsector) REFERENCES subsector(id),
+  FOREIGN KEY (id_grade) REFERENCES grade(id)
+);
+
+
+
 
 CREATE TABLE "user" (
-  "id" integer PRIMARY KEY,
-  "fname" varchar,
-  "lname" varchar,
-  "email" varchar,
-  "phone" varchar,
-  "adress" varchar,
-  "pwd" varchar,
-  "age" integer
+  id SERIAL PRIMARY KEY,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  phone VARCHAR(20),
+  address TEXT,
+  pwd VARCHAR(255) NOT NULL,
+  age INTEGER Check (age >0)
 );
 
-CREATE TABLE "boulder" (
-  "id" integer PRIMARY KEY,
-  "id_subsector" integer,
-  "id_grade" integer,
-  "condition" integer,
-  "coordinates" varchar
+CREATE TABLE boulder_climbed (
+  id SERIAL PRIMARY KEY,
+  id_user INTEGER,
+  id_boulder INTEGER,
+  date DATE,
+  comments TEXT,
+  FOREIGN KEY (id_user) REFERENCES "user"(id),
+  FOREIGN KEY (id_boulder) REFERENCES boulder(id)
 );
 
-CREATE TABLE "subsector" (
-  "id" integer PRIMARY KEY,
-  "id_sector" integer,
-  "name" varchar,
-  "description" varchar
+CREATE TABLE ranking (
+  id SERIAL PRIMARY KEY,
+  id_user INTEGER,
+  score INTEGER,
+  FOREIGN KEY (id_user) REFERENCES "user"(id)
 );
-
-CREATE TABLE "boulder_climbed" (
-  "id" integer PRIMARY KEY,
-  "id_user" integer,
-  "id_boulder" integer,
-  "date" DATE,
-  "comments" varchar
-);
-
-CREATE TABLE "ranking" (
-  "id" integer PRIMARY KEY,
-  "id_user" integer,
-  "score" integer
-);
-
-CREATE TABLE "grade" (
-  "id" integer PRIMARY KEY,
-  "grade_score" varchar,
-  "name" varchar
-);
-
-ALTER TABLE "boulder" ADD FOREIGN KEY ("id_subsector") REFERENCES "subsector" ("id");
-
-ALTER TABLE "subsector" ADD FOREIGN KEY ("id_sector") REFERENCES "sector" ("id");
-
-ALTER TABLE "boulder" ADD FOREIGN KEY ("id") REFERENCES "boulder_climbed" ("id_boulder");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "boulder_climbed" ("id_user");
-
-ALTER TABLE "grade" ADD FOREIGN KEY ("id") REFERENCES "boulder" ("id_grade");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "ranking" ("id_user");
